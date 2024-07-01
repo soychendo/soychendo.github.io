e4071aac62dd3a7715464173277b1b4599b15b25
chendodev <chendodev@gmail.com>
2024-06-30 22:14:09 -0500

    Updated Styles of social media icons

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -12,7 +12,6 @@ const Header = (): JSX.Element => {
           <ul>
             <li><a href="https://paypal.me/chendoec?country.x=EC&locale.x=es_XC" target="_blank">PayPal</a></li>
             <li><a href="https://www.patreon.com/chendo" target="_blank">Patreon</a></li>
-            <li><a href="https://www.youtube.com/@soy.chendo" target="_blank">Dev</a></li>
           </ul>
         </nav>
       </div>
