958c16789c2bc282249763681e5f102ec348f228
chendodev <chendodev@gmail.com>
2025-01-29 13:06:17 -0500

    fix src link mobile

--- src/components/header/Mobile/index.tsx
+++ src/components/header/Mobile/index.tsx
@@ -3,12 +3,12 @@ import { Nav, Link } from './styles'
 import { MdHome, MdSubject, MdForwardToInbox, MdOutlineViewInAr, MdSettingsEthernet } from 'react-icons/md'
 
 const MobileNav = (): JSX.Element => {
-  const CODE = 'https://github.com/soychendo/developer';
+  const CODE = 'https://github.com/soychendo/soychendo.github.io';
   const SIZE = '32px'
 
   return (
     <Nav>
-      <Link to='/developer'><MdHome size={SIZE} /></Link>
+      <Link to='/'><MdHome size={SIZE} /></Link>
       <Link to='/about'><MdSubject size={SIZE} /></Link>
       <Link to='/contact'><MdForwardToInbox size={SIZE} /></Link>
       <Link to='/portfolio'><MdOutlineViewInAr size={SIZE} /></Link>
